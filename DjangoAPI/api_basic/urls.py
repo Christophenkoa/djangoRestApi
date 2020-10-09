0afffbaad4e1d1ec4479f73d842412d39c68bf62
Christophenkoa <nkoachristophe@gmail.com>
2020-10-09 13:17:13 +0100

    viewset

--- DjangoAPI/api_basic/urls.py
+++ DjangoAPI/api_basic/urls.py
@@ -1,9 +1,15 @@
-from django.urls import path
-from .views import article_list, ArticleAPIView, GenericAPIView
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import article_list, ArticleAPIView, GenericAPIView, ArticleViewSet
+
+router = DefaultRouter()
+router.register('article', ArticleViewSet, basename='article')
 
 urlpatterns = [
     # path('article/', article_list),
-    path('article/', ArticleAPIView.as_view()),
+    path('viewset/', include(router.urls)),
+    path('viewset/', include(router.urls)),
+    path('viewset/<int:pk>/', include(router.urls)),
     path('article/<int:id>/', ArticleAPIView.as_view()),
     path('generic/article/<int:id>/', GenericAPIView.as_view()),
     path('generic/article/', GenericAPIView.as_view()),
